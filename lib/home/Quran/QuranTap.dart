05adaed7563ea331c60aa3471cc7437673a24c0e
john safwat <john362sss@gmail.com>
2022-11-27 01:17:48 +0200

    add all the islami project file % the qurantap file

--- lib/home/Quran/QuranTap.dart
+++ lib/home/Quran/QuranTap.dart
@@ -11,43 +11,8 @@ class QuranTap extends StatelessWidget {
           child: Image.asset('assets/images/quran_image.png'),
           alignment: Alignment.topCenter,
         ),
-        Container(
-          width: double.infinity,
-          color: MyTheme.primaryColor,
-          margin:const EdgeInsets.only(top: 5),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Expanded(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 2 , vertical: 5),
-                    padding: EdgeInsets.all(7),
-                    color: Colors.white,
-                    child: const Text("عدد الايات" ,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  )
-              ),
-              Expanded(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 2 , vertical: 5),
-                    padding: EdgeInsets.all(7),
-                    color: Colors.white,
-                    child: const Text("اسم السورة" ,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  )
-              ),
-            ],
-          ),
+        Row(
+
         ),
       ],
     );
